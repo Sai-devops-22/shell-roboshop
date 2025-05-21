ed1a3e7b0fafaa43969ec72a759fa62de8f34271
Sai-devops-22 <badesaivenkat@gmail.com>
2025-05-21 11:36:35 +0530

    new

--- roboshop.sh
+++ roboshop.sh
@@ -9,11 +9,11 @@ DOMAIN_NAME="dpractice.site"
 for instances in ${INSTANCES(@)}
 do
     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids ssg-028aa04b4b0793ed4 --tag-specifications "ResourceType=instance,
-    Tags=[{Key=Name, Value=test}]" --query "Instances[0].InstancesId" --output text)
+    Tags=[{Key=Name, Value=$instances}]" --query "Instances[0].InstancesId" --output text)
     if [ $instance != "frontend" ]
     then    
-        IP=aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
     else
-        IP=aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
     echo "$instance : $IP"
 done
