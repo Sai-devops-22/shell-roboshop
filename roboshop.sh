ba6b119c38f88616dc1cc76fb47f006962aa9392
Sai-devops-22 <badesaivenkat@gmail.com>
2025-05-23 23:23:15 +0530

    msg

--- roboshop.sh
+++ roboshop.sh
@@ -14,7 +14,7 @@ do
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
-        echo "$instance ip address is : $IP"
     fi
+    echo "$instance ip address is : $IP"
 done
 
