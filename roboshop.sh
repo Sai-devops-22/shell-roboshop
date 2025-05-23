0e918c2668293b68c0c5b4819addb2df73f3a512
Sai-devops-22 <badesaivenkat@gmail.com>
2025-05-23 23:12:56 +0530

    msg

--- roboshop.sh
+++ roboshop.sh
@@ -8,7 +8,7 @@ echo "the new way"
 
 for instance in ${INSTANCES[@]}
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-028aa04b4b0793ed4 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-028aa04b4b0793ed4 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance -ne "frontend" ]
     then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
@@ -17,3 +17,4 @@ do
         echo "$instance ip address is : $IP"
     fi
 done
+
