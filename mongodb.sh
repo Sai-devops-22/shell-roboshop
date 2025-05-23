6045af8b1c9b7a8691ce8fd4801b732715c662a4
Sai-devops-22 <badesaivenkat@gmail.com>
2025-05-24 00:57:32 +0530

    msg

--- mongodb.sh
+++ mongodb.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-USER_ID=$(id -u)
-LOG_FOLDER="var/log/shellroboshop-log"
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shellroboshop-log"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
-mkdir -p $LOG_FILE
-echo "THE EXECUTION START TIME:$(date)" | tee -a $LOG_FILE
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+mkdir -p $LOGS_FOLDER
 
 if [ $USER_ID -ne 0 ]
 then 
