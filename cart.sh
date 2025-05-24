c5153159692d766a9d986d0799fa8fe19329da76
Sai-devops-22 <badesaivenkat@gmail.com>
2025-05-24 12:51:30 +0530

    msg

---
+++ cart.sh
@@ -0,0 +1,70 @@
+START_TIME=$(date +%s)
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shellroboshop-log"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_DIRCT=$PWD
+
+mkdir -p $LOGS_FOLDER
+
+if [ $USER_ID -ne 0 ]
+then 
+    echo "login to root user and try again" | tee -a $LOG_FILE
+    exit 1
+else
+    echo "you are a root user" | tee -a $LOG_FILE
+fi
+
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then    
+        echo "the $2 is SUCCCES..." | tee -a $LOG_FILE
+    else
+        echo "the $2 is FAILED....." | tee -a $LOG_FILE
+    fi
+}
+
+dnf module disable nodejs -y &>>$LOG_FILE
+VALIDATE $? "DISABLING EXSISTING FILE"
+
+dnf module enable nodejs:20 -y &>>$LOG_FILE
+VALIDATE $? "ENABLING NODE JS : 20"
+
+dnf install nodejs -y &>>$LOG_FILE
+VALIDATE $? "INSTALLING NODEJS"
+
+id roboshop
+if [ $? -ne 0 ]
+then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+else
+    echo "already exist"
+fi
+
+mkdir -p /app 
+VALIDATE $? "creating directory"
+
+curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip
+VALIDATE $? "DOWMLODING FILE"
+
+rm -rf /app/*
+cd /app 
+VALIDATE $? "CHAGING TO APP FOLDER"
+
+unzip /tmp/cart.zip &>>$LOG_FILE
+VALIDATE $? "UNZIPPING"
+
+npm install &>>$LOG_FILE
+VALIDATE $? "INSTALLING DEPENDENCY"
+
+cp $SCRIPT_DIRCT/cart.service /etc/systemd/system/cart.service &>>$LOG_FILE
+VALIDATE $? "COPYING FILE"
+
+systemctl daemon-reload &>>$LOG_FILE
+VALIDATE $? "SYSTEM RESTARTING"
+
+systemctl enable cart &>>$LOG_FILE
+VALIDATE $? "ENABLING CART"
+
+systemctl start cart &>>$LOG_FILE
+VALIDATE $? "STARTING CART"
